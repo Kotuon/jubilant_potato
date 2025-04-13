5b41146dfd30e447aeb71accbcf2fd051d68c0aa
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2025-04-13 12:15:16 -0700

    Clean up some of the old unused code. Added comments into some of the functions.

--- Source/jubilant_potato/GravMovementComponent.cpp
+++ Source/jubilant_potato/GravMovementComponent.cpp
@@ -5,9 +5,6 @@
 #include "GravPlayerCharacter.h"
 
 void UGravMovementComponent::BeginPlay() {
-    // LastGravityToWorldTransform = GetGravityToWorldTransform();
-    // LastWorldToGravityTransform = GetWorldToGravityTransform();
-
     CharacterOwner->MovementModeChangedDelegate.AddUniqueDynamic(
         this, &UGravMovementComponent::MovementModeChanged );
 
@@ -62,31 +59,39 @@ bool UGravMovementComponent::ShouldRemainVertical() const {
 void UGravMovementComponent::UpdateRotation( float DeltaTime ) {
     if ( MovementMode == MOVE_Falling &&
          ( !hasUpdatedRotationForNewGravity || currentlyUpdatingRotation ) ) {
+
+        // Search ray in direction of gravity
         FHitResult hitResult;
         const FVector start = GetActorLocation();
         const FVector end = start + GetGravityDirection() * 400.f;
         bool overLand = GetWorld()->LineTraceSingleByChannel(
             hitResult, start, end, ECollisionChannel::ECC_Visibility );
 
+        // Rotate player model to match floor direction
         if ( overLand ) {
             DrawDebugLine( GetWorld(), start, end, FColor::Red, false, 0.f,
                            ( uint8 )0U, 2.f );
 
+            // Calculate rotation delta based on distance from floor
             const float alpha =
                 1.f - FMath::Clamp( ( ( hitResult.Distance - 20.f ) / 400.f ),
                                     0.f, 1.f );
 
+            // Get the new rotation
             const FQuat newRotation =
                 FQuat::FastLerp( currRotation, desiredRotation, alpha )
                     .GetNormalized();
 
+            // Update character rotation
             CharacterOwner->SetActorRotation( newRotation );
 
+            // Update flags
             if ( !hasUpdatedRotationForNewGravity ) {
                 hasUpdatedRotationForNewGravity = true;
                 currentlyUpdatingRotation = true;
             }
 
+            // Check if player has rotated enough
             if ( ( hitResult.Distance / ( 400.f ) ) <= 0.f )
                 currentlyUpdatingRotation = false;
 
@@ -98,44 +103,25 @@ void UGravMovementComponent::UpdateRotation( float DeltaTime ) {
 }
 
 void UGravMovementComponent::SetGravityDirection( const FVector& GravityDir ) {
+    // Local reference to current gravity
     const FVector& currGravityDir = GetGravityDirection();
 
+    // Checking if the gravity has changed
     if ( GravityDir.Equals( currGravityDir ) ) return;
 
-    // const FQuat WorldToNegativeGravityTransform =
-    //     FQuat::FindBetweenNormals( FVector::UpVector, GravityDir );
-
-    // const FQuat NegativeGravityToWorldTransform =
-    //     WorldToNegativeGravityTransform.Inverse();
-
-    // currRotation = CharacterOwner->GetActorQuat();
-
-    // if ( hasUpdatedRotationForNewGravity ) {
-    //     currLastGravRotation = ( GetWorldToGravityTransform() * currRotation
-    //     );
-    // }
-
-    // desiredRotation =
-    //     ( NegativeGravityToWorldTransform * currLastGravRotation );
-
+    // Calculate delta rotaiton between current and new gravity
     const FQuat deltaRot =
         FQuat::FindBetweenVectors( currGravityDir, GravityDir ).GetNormalized();
 
     currRotation = CharacterOwner->GetActorQuat();
 
+    // Calculate new rotation
     desiredRotation = deltaRot * currRotation;
 
+    // Update flags
     hasUpdatedRotationForNewGravity = false;
     gravIsDirty = true;
 
     Super::SetGravityDirection( GravityDir );
     //...
 }
-
-// const FQuat& UGravMovementComponent::GetLastGravityToWorldTransform() const {
-//     return LastGravityToWorldTransform;
-// }
-
-// const FQuat& UGravMovementComponent::GetLastWorldToGravityTransform() const {
-//     return LastWorldToGravityTransform;
-// }
